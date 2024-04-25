b8eea21e291874006972e33f787ea25f9b844c43
1st <styin8@gmail.com>
2024-04-25 20:04:32 +0800

    rm scripts

--- scripts/compress_game_js.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-JS_PATH=/Users/1st/acapp/game/static/js/
-JS_PATH_SRC=${JS_PATH}src/
-JS_PATH_DIST=${JS_PATH}dist/
-
-find ${JS_PATH_SRC} -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
